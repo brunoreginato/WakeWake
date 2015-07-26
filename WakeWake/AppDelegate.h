346a24fb19122281fb0089d2637fafc2e466aab1
Bruno Isola Reginato <brunoisola@ciandt.com>
2015-07-26 18:28:16 -0300

    Initial Commit

---
+++ WakeWake/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WakeWake
+//
+//  Created by Bruno Isola Reginato on 7/26/15.
+//  Copyright (c) 2015 BReginato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ WakeWake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WakeWake
+//
+//  Created by Bruno Isola Reginato on 7/26/15.
+//  Copyright (c) 2015 BReginato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
